9f5426fe09ee2497a6e1c32fae68d6463dbad13a
Jakub Kozłowski <kubukoz@gmail.com>
2021-05-29 01:23:41 +0200

    aa

--- library/src/main/scala/cats/effect/kernel/Async.scala
+++ library/src/main/scala/cats/effect/kernel/Async.scala
@@ -9,25 +9,29 @@ import java.util.concurrent.atomic.AtomicReference
 import scala.annotation.tailrec
 import scala.concurrent.{ExecutionContext, Future}
 
-trait Async2[F[_]] {
-
-  def cont[K, R](body: Cont[F, K, R]): F[R]
-}
+trait Async2[F[_]] {}
 
 object Async2 {
   def apply[F[_]](implicit F: Async2[F]): F.type = F
 
   private[effect] trait WriterTAsync[F[_], X]
       extends Async2[WriterT[F, X, *]]
-      with Sync.WriterTSync[F, X] {
+      with WriterTSync[F, X] {
 
     implicit protected def F: Sync[F] with GenTemporal[F, Throwable] = ???
 
     override protected final def delegate = super.delegate
     override protected final def C = ???
 
-    override def unique: WriterT[F, X, Unique.Token] =
-      delay(new Unique.Token())
+    override def unique: WriterT[F, X, Unique.Token] = ???
 
   }
+
+  private[effect] trait WriterTSync[F[_], S]
+      extends Sync[WriterT[F, S, *]]
+      with MonadCancel.WriterTMonadCancel[F, S, Throwable]
+      with Clock.WriterTClock[F, S] {
+    implicit protected def F: MonadCancelThrow[F]
+  }
+
 }
